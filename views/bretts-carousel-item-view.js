e4e221e9c29aa942b4068f33aed93ec711f19eee
Brett Bisyak <brettbgb@users.noreply.github.com>
2016-08-06 22:29:25 -0700

    Update bretts-carousel-item-view.js

--- views/bretts-carousel-item-view.js
+++ views/bretts-carousel-item-view.js
@@ -1,17 +1,17 @@
 	var CarouselItemView = Backbone.View.extend({
 		
-		tagName: 'li',
+	    tagName: 'li',
 
-		initialize: function(){
-		    this.render();
-		},
+	    initialize: function(){
+                this.render();
+	    },
 		
-        render: function(){
-			var template = _.template($( '#carouselItemTemplate' ).html(), {songArtworkUrl:this.model.get('songArtworkUrl'), 
-			                                                                songTitle:this.model.get('songTitle'),
-																			artistName: this.model.get('artistName')});
-			this.$el.html(template);
-			return this;
-        }
+            render: function(){
+	        var template = _.template($( '#carouselItemTemplate' ).html(), {songArtworkUrl:this.model.get('songArtworkUrl'), 
+			                                                        songTitle:this.model.get('songTitle'),
+										artistName: this.model.get('artistName')});
+		this.$el.html(template);
+		return this;
+            }
 	});
 	
